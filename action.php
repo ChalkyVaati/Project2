5acca0af65c2ae717b28a96dc370e219380ac80a
skyriser92 <35499411+skyriser92@users.noreply.github.com>
2018-04-20 09:47:53 -0400

    added check and delete functions

--- action.php
+++ action.php
@@ -2,13 +2,26 @@
 session_start();
 
 if(isset($_POST['deleteItem'])) {
-	global $db;
-	$delete = $_POST['deleteItem'];
-    $query = 'DELETE FROM todos
-              WHERE id = '$delete'';
-    $statement = $db->prepare($query);
-    $statement->execute();
-    $statement->closeCursor();
+		
+	$servername = "sql2.njit.edu";
+	$username = "jcm44";
+	$password = "lq40ntX5";
+	$dbname = "jcm44";
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+		//echo "Connected Successfully! <br>";
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		
+		$email = $_SESSION["email"];
+		$delete = $_POST['deleteItem'];
+
+		$query = "DELETE FROM todos WHERE id = '$delete'";
+		$statement = $conn->prepare($query);
+		$statement->execute();
+    } 
+	catch(PDOException $e) {
+		echo "Error: " . $e->getMessage();
+	}
 }
 
 if(isset($_POST['EditItem'])) {
@@ -16,12 +29,25 @@ if(isset($_POST['EditItem'])) {
 }
 
 if(isset($_POST['CheckItem'])) {
-	$email = $_SESSION["email"];
-	global $db;
-	$delete = $_POST['deleteItem'];
-    $query = 'UPDATE todos SET isdone = 1 WHERE owneremail = '$email'';
-    $statement = $db->prepare($query);
-    $statement->execute();
+	$servername = "sql2.njit.edu";
+	$username = "jcm44";
+	$password = "lq40ntX5";
+	$dbname = "jcm44";
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+		//echo "Connected Successfully! <br>";
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	
+		$email = $_SESSION["email"];
+		$isdone = 1;
+		$check = $_POST['CheckItem'];
+		$query = "UPDATE todos (isdone) VALUES ('$isdone') WHERE owneremail = '$email' AND id = '$check'";
+		$statement = $db->prepare($query);
+		$statement->execute();
+	}
+	catch(PDOException $e) {
+		echo "Error: " . $e->getMessage();
+	}
 }
 
 ?>
